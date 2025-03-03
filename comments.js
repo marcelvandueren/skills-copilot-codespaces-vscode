cc9b55460032f05a146acdc4e3db83dd32cf474e
marcelvandueren <marcel.vandueren@web.de>
2025-03-03 15:32:55 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,14 @@
+// Create web server
+// Create a web server that listens to incoming requests on port 3000. The server should respond with "Hello, World!".
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(3000, '127.0.0.1', () => {
+  console.log('Server running at http://127.0.0.1:3000/');
+});
